fc4f2077d3222520b981458b079c873498c5dade
Maksym-Kartawcew <maxkartavtsev@gmail.com>
2024-01-29 00:04:14 +0100

    update mongoDB

--- utils/database.js
+++ utils/database.js
@@ -13,14 +13,12 @@ export const connectToDB = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URI, {
       dbName: "share_prompt",
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
     });
 
     isConnected = true;
 
     console.log("MongoDB connected");
   } catch (error) {
-    console.log(error);
+    console.log("Failed to connect to MongoDB", error);
   }
 };
