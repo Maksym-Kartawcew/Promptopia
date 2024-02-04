39c24eecae7ea5a2433199e2c3162ac8a5b02ce9
Maksym-Kartawcew <maxkartavtsev@gmail.com>
2024-02-04 13:43:20 +0100

    feed in homepage

--- app/page.jsx
+++ app/page.jsx
@@ -1,19 +1,114 @@
-import Feed from "@components/Feed";
+"use client";
+import React, { useState, useEffect } from "react";
+import PromptCard from "@components/PromptCard";
 
-const Home = () => (
-  <section className="w-full flex-center flex-col">
-    <h1 className="head_text text-center">
-      Discover & Share
-      <br className="max-md:hidden" />
-      <span className="orange_gradient text-center"> AI-Powered Prompts</span>
-    </h1>
-    <p className="desc text-center">
-      Promptopia is an open-source AI prompting tool for modern world to
-      discover, create and share creative prompts
-    </p>
+const PromptCardList = ({ data, handleTagClick }) => {
+  return (
+    <div className="mt-16 prompt_layout">
+      {data.map((post) => (
+        <PromptCard
+          key={post._id}
+          post={post}
+          handleTagClick={handleTagClick}
+        />
+      ))}
+    </div>
+  );
+};
 
-    <Feed />
-  </section>
-);
+const Home = () => {
+  const [allPosts, setAllPosts] = useState([]);
+  const [searchText, setSearchText] = useState("");
+  const [searchTimeout, setSearchTimeout] = useState(null);
+  const [searchedResults, setSearchedResults] = useState([]);
+
+  const fetchPosts = async () => {
+    try {
+      const response = await fetch("/api/prompt");
+      if (response.ok) {
+        const data = await response.json();
+        setAllPosts(data);
+      } else {
+        console.error("Failed to fetch prompts");
+      }
+    } catch (error) {
+      console.error("Error fetching prompts:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  const filterPrompts = (searchtext) => {
+    const regex = new RegExp(searchtext, "i");
+    return allPosts.filter(
+      (item) =>
+        regex.test(item.creator.username) ||
+        regex.test(item.tag) ||
+        regex.test(item.prompt)
+    );
+  };
+
+  const handleSearchChange = (e) => {
+    clearTimeout(searchTimeout);
+    setSearchText(e.target.value);
+
+    setSearchTimeout(
+      setTimeout(() => {
+        const searchResult = filterPrompts(e.target.value);
+        setSearchedResults(searchResult);
+      }, 500)
+    );
+  };
+
+  const handleTagClick = (tagName) => {
+    setSearchText(tagName);
+
+    const searchResult = filterPrompts(tagName);
+    setSearchedResults(searchResult);
+  };
+
+  return (
+    <>
+      <section className="w-full flex-center flex-col">
+        <h1 className="head_text text-center">
+          Discover & Share
+          <br className="max-md:hidden" />
+          <span className="orange_gradient text-center">
+            {" "}
+            AI-Powered Prompts
+          </span>
+        </h1>
+        <p className="desc text-center">
+          Promptopia is an open-source AI prompting tool for modern world to
+          discover, create and share creative prompts
+        </p>
+      </section>
+
+      <section className="feed">
+        <form className="relative w-full flex-center">
+          <input
+            type="text"
+            placeholder="Search for a tag or a username"
+            value={searchText}
+            onChange={handleSearchChange}
+            required
+            className="search_input peer"
+          />
+        </form>
+
+        {searchText ? (
+          <PromptCardList
+            data={searchedResults}
+            handleTagClick={handleTagClick}
+          />
+        ) : (
+          <PromptCardList data={allPosts} handleTagClick={handleTagClick} />
+        )}
+      </section>
+    </>
+  );
+};
 
 export default Home;
